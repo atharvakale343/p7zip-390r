59e85dce6b8ef90fdc8a08a5a98a7fbb03b7b806
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2023-05-14 22:20:08 -0400

    Changed file

--- fuzzing/harness.cpp
+++ fuzzing/harness.cpp
@@ -30,170 +30,172 @@
 
 using namespace NWindows;
 
-extern
-CStdOutStream *g_StdStream;
+extern CStdOutStream *g_StdStream;
 CStdOutStream *g_StdStream = NULL;
-extern
-CStdOutStream *g_ErrStream;
+extern CStdOutStream *g_ErrStream;
 CStdOutStream *g_ErrStream = NULL;
 
 extern int Main2(
-  #ifndef _WIN32
-  int numArgs, char *args[]
-  #endif
+#ifndef _WIN32
+    int numArgs, char *args[]
+#endif
 );
 
-static const char * const kException_CmdLine_Error_Message = "Command Line Error:";
-static const char * const kExceptionErrorMessage = "ERROR:";
-static const char * const kUserBreakMessage  = "Break signaled";
-static const char * const kMemoryExceptionMessage = "ERROR: Can't allocate required memory!";
-static const char * const kUnknownExceptionMessage = "Unknown Error";
-static const char * const kInternalExceptionMessage = "\n\nInternal Error #";
+static const char *const kException_CmdLine_Error_Message = "Command Line Error:";
+static const char *const kExceptionErrorMessage = "ERROR:";
+static const char *const kUserBreakMessage = "Break signaled";
+static const char *const kMemoryExceptionMessage = "ERROR: Can't allocate required memory!";
+static const char *const kUnknownExceptionMessage = "Unknown Error";
+static const char *const kInternalExceptionMessage = "\n\nInternal Error #";
 
 static void FlushStreams()
 {
-  if (g_StdStream)
-    g_StdStream->Flush();
+    if (g_StdStream)
+        g_StdStream->Flush();
 }
 
 static void PrintError(const char *message)
 {
-  FlushStreams();
-  if (g_ErrStream)
-    *g_ErrStream << "\n\n" << message << endl;
+    FlushStreams();
+    if (g_ErrStream)
+        *g_ErrStream << "\n\n"
+                     << message << endl;
 }
 
 #if defined(_WIN32) && defined(_UNICODE) && !defined(_WIN64) && !defined(UNDER_CE)
-#define NT_CHECK_FAIL_ACTION *g_StdStream << "Unsupported Windows version"; return NExitCode::kFatalError;
+#define NT_CHECK_FAIL_ACTION                       \
+    *g_StdStream << "Unsupported Windows version"; \
+    return NExitCode::kFatalError;
 #endif
 
-int MY_CDECL main_7zz
-(
-  #ifndef _WIN32
-  int numArgs, char *args[]
-  #endif
+int MY_CDECL main_7zz(
+#ifndef _WIN32
+    int numArgs, char *args[]
+#endif
 )
 {
-  g_ErrStream = &g_StdErr;
-  g_StdStream = &g_StdOut;
+    g_ErrStream = &g_StdErr;
+    g_StdStream = &g_StdOut;
 
-  NT_CHECK
+    NT_CHECK
 
-  NConsoleClose::CCtrlHandlerSetter ctrlHandlerSetter;
-  int res = 0;
-  
-  try
-  {
-    #ifdef _WIN32
-    My_SetDefaultDllDirectories();
-    #endif
+    NConsoleClose::CCtrlHandlerSetter ctrlHandlerSetter;
+    int res = 0;
 
-    res = Main2(
-    #ifndef _WIN32
-    numArgs, args
-    #endif
-    );
-  }
-  catch(const CNewException &)
-  {
-    PrintError(kMemoryExceptionMessage);
-    return (NExitCode::kMemoryError);
-  }
-  catch(const NConsoleClose::CCtrlBreakException &)
-  {
-    PrintError(kUserBreakMessage);
-    return (NExitCode::kUserBreak);
-  }
-  catch(const CMessagePathException &e)
-  {
-    PrintError(kException_CmdLine_Error_Message);
-    if (g_ErrStream)
-      *g_ErrStream << e << endl;
-    return (NExitCode::kUserError);
-  }
-  catch(const CSystemException &systemError)
-  {
-    if (systemError.ErrorCode == E_OUTOFMEMORY)
+    try
     {
-      PrintError(kMemoryExceptionMessage);
-      return (NExitCode::kMemoryError);
-    }
-    if (systemError.ErrorCode == E_ABORT)
-    {
-      PrintError(kUserBreakMessage);
-      return (NExitCode::kUserBreak);
-    }
-    if (g_ErrStream)
-    {
-      PrintError("System ERROR:");
-      *g_ErrStream << NError::MyFormatMessage(systemError.ErrorCode) << endl;
-    }
-    return (NExitCode::kFatalError);
-  }
-  catch(NExitCode::EEnum &exitCode)
-  {
-    FlushStreams();
-    if (g_ErrStream)
-      *g_ErrStream << kInternalExceptionMessage << exitCode << endl;
-    return (exitCode);
-  }
-  catch(const UString &s)
-  {
-    if (g_ErrStream)
-    {
-      PrintError(kExceptionErrorMessage);
-      *g_ErrStream << s << endl;
-    }
-    return (NExitCode::kFatalError);
-  }
-  catch(const AString &s)
-  {
-    if (g_ErrStream)
-    {
-      PrintError(kExceptionErrorMessage);
-      *g_ErrStream << s << endl;
-    }
-    return (NExitCode::kFatalError);
-  }
-  catch(const char *s)
-  {
-    if (g_ErrStream)
-    {
-      PrintError(kExceptionErrorMessage);
-      *g_ErrStream << s << endl;
-    }
-    return (NExitCode::kFatalError);
-  }
-  catch(const wchar_t *s)
-  {
-    if (g_ErrStream)
-    {
-      PrintError(kExceptionErrorMessage);
-      *g_ErrStream << s << endl;
-    }
-    return (NExitCode::kFatalError);
-  }
-  catch(int t)
-  {
-    if (g_ErrStream)
-    {
-      FlushStreams();
-      *g_ErrStream << kInternalExceptionMessage << t << endl;
-      return (NExitCode::kFatalError);
-    }
-  }
-  catch(...)
-  {
-    PrintError(kUnknownExceptionMessage);
-    return (NExitCode::kFatalError);
-  }
+#ifdef _WIN32
+        My_SetDefaultDllDirectories();
+#endif
 
-  return res;
+        res = Main2(
+#ifndef _WIN32
+            numArgs, args
+#endif
+        );
+    }
+    catch (const CNewException &)
+    {
+        PrintError(kMemoryExceptionMessage);
+        return (NExitCode::kMemoryError);
+    }
+    catch (const NConsoleClose::CCtrlBreakException &)
+    {
+        PrintError(kUserBreakMessage);
+        return (NExitCode::kUserBreak);
+    }
+    catch (const CMessagePathException &e)
+    {
+        PrintError(kException_CmdLine_Error_Message);
+        if (g_ErrStream)
+            *g_ErrStream << e << endl;
+        return (NExitCode::kUserError);
+    }
+    catch (const CSystemException &systemError)
+    {
+        if (systemError.ErrorCode == E_OUTOFMEMORY)
+        {
+            PrintError(kMemoryExceptionMessage);
+            return (NExitCode::kMemoryError);
+        }
+        if (systemError.ErrorCode == E_ABORT)
+        {
+            PrintError(kUserBreakMessage);
+            return (NExitCode::kUserBreak);
+        }
+        if (g_ErrStream)
+        {
+            PrintError("System ERROR:");
+            *g_ErrStream << NError::MyFormatMessage(systemError.ErrorCode) << endl;
+        }
+        return (NExitCode::kFatalError);
+    }
+    catch (NExitCode::EEnum &exitCode)
+    {
+        FlushStreams();
+        if (g_ErrStream)
+            *g_ErrStream << kInternalExceptionMessage << exitCode << endl;
+        return (exitCode);
+    }
+    catch (const UString &s)
+    {
+        if (g_ErrStream)
+        {
+            PrintError(kExceptionErrorMessage);
+            *g_ErrStream << s << endl;
+        }
+        return (NExitCode::kFatalError);
+    }
+    catch (const AString &s)
+    {
+        if (g_ErrStream)
+        {
+            PrintError(kExceptionErrorMessage);
+            *g_ErrStream << s << endl;
+        }
+        return (NExitCode::kFatalError);
+    }
+    catch (const char *s)
+    {
+        if (g_ErrStream)
+        {
+            PrintError(kExceptionErrorMessage);
+            *g_ErrStream << s << endl;
+        }
+        return (NExitCode::kFatalError);
+    }
+    catch (const wchar_t *s)
+    {
+        if (g_ErrStream)
+        {
+            PrintError(kExceptionErrorMessage);
+            *g_ErrStream << s << endl;
+        }
+        return (NExitCode::kFatalError);
+    }
+    catch (int t)
+    {
+        if (g_ErrStream)
+        {
+            FlushStreams();
+            *g_ErrStream << kInternalExceptionMessage << t << endl;
+            return (NExitCode::kFatalError);
+        }
+    }
+    catch (...)
+    {
+        PrintError(kUnknownExceptionMessage);
+        return (NExitCode::kFatalError);
+    }
+
+    return res;
 }
 
-void write_chunk_to_file(const char *filename, const unsigned char *chunk, size_t size) {
+void write_chunk_to_file(const char *filename, const unsigned char *chunk, size_t size)
+{
     FILE *file = fopen(filename, "wb");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("Failed to create input file");
         exit(1);
     }
@@ -201,18 +203,22 @@ void write_chunk_to_file(const char *filename, const unsigned char *chunk, size_
     fclose(file);
 }
 
-void generate_random_filename(char *filename, size_t max_length) {
+void generate_random_filename(char *filename, size_t max_length)
+{
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     size_t charset_length = sizeof(charset) - 1;
 
-    for (size_t i = 0; i < max_length - 1; ++i) {
+    for (size_t i = 0; i < max_length - 1; ++i)
+    {
         filename[i] = charset[rand() % charset_length];
     }
     filename[max_length - 1] = '\0';
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
         return 1;
     }
@@ -221,7 +227,8 @@ int main(int argc, char *argv[]) {
 
     // Open the input file in binary mode
     FILE *input_file = fopen(argv[1], "rb");
-    if (input_file == NULL) {
+    if (input_file == NULL)
+    {
         perror("Failed to open input file");
         return 1;
     }
@@ -234,7 +241,8 @@ int main(int argc, char *argv[]) {
     char output_files[256][256];
     int file_count = 0;
 
-    while ((input_size = fread(input_bytes, sizeof(unsigned char), CHUNK_SIZE, input_file)) > 0) {
+    while ((input_size = fread(input_bytes, sizeof(unsigned char), CHUNK_SIZE, input_file)) > 0)
+    {
         // Generate a unique random file name for the input chunk
         char output_file[256];
         generate_random_filename(output_file, 16);
@@ -250,15 +258,21 @@ int main(int argc, char *argv[]) {
     fclose(input_file);
 
     // Build the command with the list of files
-    char *exec_file = "../7zz_afl_asan/CPP/7zip/Bundles/Alone2/_o/bin/7zz";
+    char *exec_file =  (char*) "../7zz_afl_asan/CPP/7zip/Bundles/Alone2/_o/bin/7zz";
 
-    char** command = (char**)malloc((file_count + 5) * sizeof(char*));
+    char **command = (char **)malloc((file_count + 5) * sizeof(char *));
     command[0] = exec_file;
-    command[1] = "a";
-    command[2] = "check.zip";
-    command[3] = "-y";
+    command[1] = (char *)"a";
 
-    for (int i = 0; i < file_count; i++) {
+    char zip_fname[256];
+    generate_random_filename(zip_fname, 16);
+    strcat(zip_fname, ".zip");
+
+    command[2] = zip_fname;
+    command[3] = (char *)"-y";
+
+    for (int i = 0; i < file_count; i++)
+    {
         command[i + 4] = output_files[i];
     }
     command[file_count + 4] = NULL;
@@ -267,8 +281,14 @@ int main(int argc, char *argv[]) {
     main_7zz(file_count + 4, command);
 
     // Delete files
-    for (int i = 0; i < file_count; i++) {
+    for (int i = 0; i < file_count; i++)
+    {
         remove(output_files[i]);
+    }
+
+    if (access(command[2], F_OK) == 0)
+    {
+        remove(command[2]);
     }
 
     free(command);
